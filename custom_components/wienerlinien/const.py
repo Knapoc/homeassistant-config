90206020936c8db9dee92946e27fcdf20c67bc1e
Knapoc <Knapoc@users.noreply.github.com>
2019-08-12 20:43:04 +0200

    remove wienerlinien

--- custom_components/wienerlinien/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants"""
-
-BASE_URL = "http://www.wienerlinien.at/ogd_realtime/monitor?rbl={}&sender={}"
-
-DEPARTURES = {
-    "first": {"key": 0, "name": "{} first departure"},
-    "next": {"key": 1, "name": "{} next departure"},
-}
